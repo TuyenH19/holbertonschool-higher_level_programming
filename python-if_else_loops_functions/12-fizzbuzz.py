0833c19f4195dd1c4dab995439ef9b7d62300717
TuyenH <thtuyen199@gmail.com>
2024-01-23 15:27:34 +0000

    modify last space

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for i in range(1, 101):
+    for i in range(1, 100):
         if i % 3 == 0 and i % 5 == 0:
             i = "Fizzbuzz"
         elif i % 3 == 0:
@@ -8,3 +8,4 @@ def fizzbuzz():
         elif i % 5 == 0:
             i = "Buzz"
         print("{} ".format(i), end='')
+    print("Buzz", end='')
