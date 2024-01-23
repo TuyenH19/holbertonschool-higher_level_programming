5e82b3b738bac720279a4dc4d7c0cefd92500296
TuyenH <thtuyen199@gmail.com>
2024-01-23 16:08:52 +0000

    modify words in capital

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,7 +2,7 @@
 def fizzbuzz():
     for i in range(1, 101):
         if i % 3 == 0 and i % 5 == 0:
-            i = "Fizzbuzz"
+            i = "FizzBuzz"
         elif i % 3 == 0:
             i = "Fizz"
         elif i % 5 == 0:
