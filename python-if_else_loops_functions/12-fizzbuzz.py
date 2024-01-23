0b501301d5c3135c9a32d1327c7ebc2992b3b454
TuyenH <thtuyen199@gmail.com>
2024-01-23 15:16:01 +0000

    prints the numbers from 1 to 100 with FizzBuzz

---
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+def fizzbuzz():
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            i = "Fizzbuzz"
+        elif i % 3 == 0:
+            i = "Fizz"
+        elif i % 5 == 0:
+            i = "Buzz"
+        print("{} ".format(i), end='')
