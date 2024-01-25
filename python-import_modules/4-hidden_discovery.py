42c0a37b505e8fdc322f29372083a9c09ac1ecf3
TuyenH <thtuyen199@gmail.com>
2024-01-25 15:04:43 +0000

    modify trailing whitespace

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -5,4 +5,4 @@ if __name__ == "__main__":
     name_list = dir(hidden_4)
     for name in name_list:
         if name[0] != '_':
-            print("{}".format(name))          
+            print("{}".format(name))
