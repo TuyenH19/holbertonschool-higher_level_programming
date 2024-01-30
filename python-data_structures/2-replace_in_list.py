8df590446e766f437b3ece8990ccb4e532be9340
TuyenH <thtuyen199@gmail.com>
2024-01-30 09:47:37 +0000

    delete loop function

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -3,7 +3,5 @@ def replace_in_list(my_list, idx, element):
     if idx < 0 or idx >= len(my_list):
         return (my_list)
     else:
-        for i in my_list:
-            if i == idx:
-                my_list[idx] = element
-            return (my_list)
+        my_list[idx] = element
+        return (my_list)
