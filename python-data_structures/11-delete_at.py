072b47591341a883cb6410c4f7a00005afb921ec
TuyenH <thtuyen199@gmail.com>
2024-01-31 10:14:09 +0000

    add condition of specific position

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
+    if idx < 0 or idx >= len(my_list) - 1:
+        pass
     for i in range(idx, len(my_list) - 1):
         my_list[i] = my_list[i + 1]
     my_list.remove(my_list[len(my_list) - 1])
