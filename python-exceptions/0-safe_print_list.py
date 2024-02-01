64a857fdce702376d76ecb4be4aee21372b21398
TuyenH <thtuyen199@gmail.com>
2024-02-01 13:54:23 +0000

    modify sytax of loop

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    for i in range(x):
-        try:
+    try:
+        for i in range(x):
             print(my_list[i], end='')
-        except IndexError:
-            x = i - 1
+    except IndexError:
+        x = i
     print('')
     return (x)
