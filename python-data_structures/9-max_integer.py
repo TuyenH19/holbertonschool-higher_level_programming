749136f7ae6a4867df02635934a956b473e5c548
TuyenH <thtuyen199@gmail.com>
2024-02-02 21:34:29 +0000

    modify the syntax

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    max_value = 0
     if my_list == "":
         return (None)
-    else:
-        for i in range(len(my_list)):
-            if my_list[i] > max_value:
-                max_value = my_list[i]
-    return (max_value)
+    new_list = my_list.copy()
+    new_list.sort()
+    return (new_list[-1])
