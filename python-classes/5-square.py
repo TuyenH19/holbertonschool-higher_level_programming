c1f54acf08cdc0eb757874ea55514464c7e4cdf5
TuyenH <thtuyen199@gmail.com>
2024-02-06 09:53:13 +0000

    5-Printing a square

---
+++ python-classes/5-square.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Square module"""
+
+
+class Square:
+    """Define class Square"""
+
+    def __init__(self, size=0):
+
+        """Constructor"""
+        self.__size = size
+
+    """Property retrieve the size"""
+    @property
+    def size(self):
+        return self.__size
+
+    """Setter to modify the size"""
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """Define method Area"""
+        return self.__size ** 2
+
+    def my_print(self):
+        """Define merthod My_print"""
+        if self.__size == 0:
+            print("")
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end='')
+                print("")
