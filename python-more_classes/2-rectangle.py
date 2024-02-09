96bdccb68ed86fd3957d0a1b31c714b2466c588a
TuyenH <thtuyen199@gmail.com>
2024-02-09 09:40:03 +0000

    modidy operator and to or

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -55,6 +55,6 @@ class Rectangle:
 
     def perimeter(self):
         """Define method the rectangle perimeter"""
-        if self.__width == 0 and self.__height == 0:
+        if self.__width == 0 or self.__height == 0:
             return 0
         return (self.__width + self.__height) * 2
