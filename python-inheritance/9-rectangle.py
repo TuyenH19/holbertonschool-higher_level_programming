a70a5d411a03bcedad51086ca99e45fc85bf0dc5
TuyenH <thtuyen199@gmail.com>
2024-02-14 14:58:03 +0000

    9. Full rectangle

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Module for Restangle class."""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """A subclass representing a rectangle."""
+    def __init__(self, width, height):
+        """Constructor."""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return '[Rectangle] ' + str(self.__width) + '/' + str(self.__height)
