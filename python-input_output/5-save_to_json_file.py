53cf0e6074faa1db8b6f7eb2b7e6bbad5ff011f7
TuyenH <thtuyen199@gmail.com>
2024-02-15 21:12:22 +0000

    add import dumps

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 """Module to save object to a file"""
-import json
+from json import dumps
 
 
 def save_to_json_file(my_obj, filename):
     """Method to write an object to a JSON file"""
-    json_string = json.dumps(my_obj)
     with open(filename, 'w', encoding='utf-8') as myFile:
-        myFile.write(json_string)
+        myFile.write(dumps(my_obj))
