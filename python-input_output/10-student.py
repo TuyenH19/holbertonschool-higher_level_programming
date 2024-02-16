7ffa7b2a313debfec79f509c6e88b62b464e8e5d
TuyenH <thtuyen199@gmail.com>
2024-02-16 19:01:08 +0000

    10. Student to JSON with filter

---
+++ python-input_output/10-student.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Module for student class"""
+
+
+class Student:
+    """Define a student"""
+    def __init__(self, first_name, last_name, age):
+        """Initialize a student
+
+        Args:
+            first_name (str): first name of a student.
+            last_name (str): last name of a student.
+            age (int): age of a student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Method retrieves a dictionary representation of a student with filter.
+        
+        If attrs is a list of strings, represent only those attribute.
+        Otherwise, all attributes must be retrieved"""
+        class_d = self.__dict__
+        sel_d = dict()
+
+        if type(attrs) is list:
+            for attr in attrs:
+                if type(attr) is not str:
+                    return class_d
+                if attr in class_d:
+                    sel_d[attr] = class_d[attr]
+            return sel_d
+        return class_d
