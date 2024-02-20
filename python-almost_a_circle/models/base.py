27630f6ccfd45fbc3f36f9dc904bad8388556ba9
TuyenH <thtuyen199@gmail.com>
2024-02-20 08:31:29 +0000

    initial class Base

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Define a base model class."""
+import json
+
+
+class Base:
+    """Represent the base model for all classes in projetc."""
+    __nb_objects = 0
+    def __init__(self, id=None):
+        """Constructor of a base"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
