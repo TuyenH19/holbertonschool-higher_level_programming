dfda882c0b4ce3ca14ce7e25611e64b88d975665
TuyenH <thtuyen199@gmail.com>
2024-02-22 08:25:34 +0000

    update staticmethod of to_json_string and classmethod save_to_file

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -13,3 +13,24 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Return the json serialization of a list of dictionary."""
+        if list_dictionaries is None:
+            return []
+        else:
+            return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Write the json string representation of list_objs to a file."""
+
+        filename = cls.__name__ + ".json"
+        with open(filename, "w") as jfile:
+            if list_objs is None:
+                jfile.write("[]")
+            else:
+                list_dicts = [o.to_dictionary() for o in list_objs]
+                jfile.write(Base.to_json_string(list_dicts))
+
