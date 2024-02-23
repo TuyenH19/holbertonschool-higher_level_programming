5c6a877751314f64161af1e93ccba4b8dcb107d7
TuyenH <thtuyen199@gmail.com>
2024-02-23 22:14:05 +0000

    update raise error for class Square

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -17,6 +17,10 @@ class Square(Rectangle):
     @size.setter
     def size(self, value):
         """Set the size value of the Square."""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value <= 0:
+            raise ValueError("width must be > 0")
         self.width = value
         self.height = value
 
