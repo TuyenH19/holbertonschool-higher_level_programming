5100f02b5a4e95a299b57f46222530dff740d3de
TuyenH <thtuyen199@gmail.com>
2024-03-19 08:49:31 +0000

    set all privileges to user

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,2 +1,3 @@
 -- Create the MySQL server user and set password
-CREATE USER 'user_0d_1'@'localhost' IDENTIFIED WITH user_0d_1_pwd BY 'password';
+CREATE USER IF NOT EXIST 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
