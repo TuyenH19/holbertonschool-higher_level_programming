bba8a04420174aee95c9dd634ed4fa19c105ddf7
TuyenH <thtuyen199@gmail.com>
2024-03-19 14:00:57 +0000

    modify alias

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,10 +1,10 @@
 -- Lists all the cities of California that can be found in the database hbtn_0d_usa
 SELECT cities.id, cities.name
 FROM cities
-WHERE cities.states_id = 
+WHERE cities.state_id = 
     (
         SELECT states.id
         FROM states
         WHERE states.name = "California"
     )
-ORDER BY c.id;
+ORDER BY cities.id;
