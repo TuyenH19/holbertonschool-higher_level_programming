20e27b38ca32bdf6ab192d3931a591a017e41dbc
TuyenH <thtuyen199@gmail.com>
2024-03-20 14:38:00 +0000

    correct from

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- Lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows
 SELECT tv_shows.title, tv_genres.name
-FROM tv_shows.title
+FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 GROUP BY tv_shows.title, tv_genres.name
