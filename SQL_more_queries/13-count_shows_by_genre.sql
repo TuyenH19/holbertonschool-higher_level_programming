01ca339528e9a1f1e21be81f14bae12fff773580
TuyenH <thtuyen199@gmail.com>
2024-03-20 10:17:14 +0000

    modify group by

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -3,6 +3,6 @@ SELECT tv_genres.name as genre, COUNT(tv_show_genres.genre_id) as number_of_show
 FROM tv_genres
     LEFT JOIN tv_show_genres 
     ON tv_genres.id = tv_show_genres.genre_id
-GROUP BY genre
+GROUP BY tv_genres.id
 HAVING number_of_shows > 0
 ORDER BY number_of_shows DESC;
