2b3396402153bed78538db99ca9a77c4b434201d
TuyenH <thtuyen199@gmail.com>
2024-03-20 15:26:26 +0000

    change to inner join

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -2,7 +2,7 @@
 SELECT tv_genres.name as genre,
         COUNT(tv_shows.id) as number_of_shows
 FROM tv_genres
-LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id AND tv_shows.title IS NOT NULL
+JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
 GROUP BY tv_genres.name
 ORDER BY number_of_shows DESC;
