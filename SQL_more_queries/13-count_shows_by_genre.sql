bbf54448458e7cf0cdefae35b3e1e57609fae6a2
TuyenH <thtuyen199@gmail.com>
2024-03-20 14:25:07 +0000

    modify condition of where part

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -4,5 +4,5 @@ SELECT tv_genres.name as genre,
 FROM tv_genres
 LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
-WHERE tv_shows.title IS NOT NULL
+WHERE number_of_shows > 0
 ORDER BY number_of_shows DESC;
