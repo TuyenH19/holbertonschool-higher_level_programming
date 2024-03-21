7ee95a1a172b9d5d519acb831bdd80a833ac8bd8
TuyenH <thtuyen199@gmail.com>
2024-03-21 22:02:42 +0000

    1. Filter states

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 A Python script that lists all states with a name starting with N
-from the database hbtn_0e_0_usa.
+from the database hbtn_0e_0_usa
 Script takes 3 arguments: mysql username, mysql password, and database name.
 Script uses the MySQLdb module to connect to a MySQL server
 running on localhost at port 3306.
