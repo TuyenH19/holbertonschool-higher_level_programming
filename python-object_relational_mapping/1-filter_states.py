09f11d5ea0120eaa51ed69131b0b2cf6b1dcab5e
TuyenH <thtuyen199@gmail.com>
2024-03-21 22:01:44 +0000

    1. Filter states

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+A Python script that lists all states with a name starting with N
+from the database hbtn_0e_0_usa.
+Script takes 3 arguments: mysql username, mysql password, and database name.
+Script uses the MySQLdb module to connect to a MySQL server
+running on localhost at port 3306.
+Results are sorted in ascending order by states.id.
+"""
+
+import MySQLdb
+import sys
+
+
+def list_states(username, password, db_name):
+    """
+    Connects to a MySQL database and lists all states from the database.
+    """
+    db = MySQLdb.connect(
+        host="localhost",
+        user=username,
+        passwd=password,
+        db=db_name,
+        port=3306
+    )
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name like 'N%' ORDER BY id ASC")
+    states = cur.fetchall()
+
+    for state in states:
+        print(state)
+
+    cur.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 4:  # Ensuring there are three arguments
+        list_states(sys.argv[1], sys.argv[2], sys.argv[3])
