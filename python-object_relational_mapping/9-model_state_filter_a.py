14f9c3f6f8b32307e66120672e3e9405f28ef1d3
TuyenH <thtuyen199@gmail.com>
2024-03-22 20:08:24 +0000

    9. Contains `a`

---
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Print state include 'a' letter
+"""
+import sys
+
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
+
+from model_state import Base, State
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'
+        .format(username, password, database), pool_pre_ping=True
+    )
+    Session = sessionmaker(bind=engine)
+    with Session() as session:
+        states = session.query(State).filter(State.name.like('%a%'))
+        for state in states:
+            print(f"{state.id}: {state.name}")
